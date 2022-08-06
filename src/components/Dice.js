447a28422744726f6f589ce63881770e1e0e21d9
Hanabata <dianneerika.busa@gmail.com>
2022-08-07 08:37:21 +0900

    CSS dice feature complete

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -6,7 +6,21 @@ function Dice(props) {
     }
 
     return (
-        <button onClick={props.holdDice} style={styles}>{props.num}</button>
+        // <button onClick={props.holdDice} style={styles}>{props.num}</button>
+        // Dice face test
+        <button className='fifth-face' onClick={props.holdDice} style={styles}>
+            <div className='column'>
+                <span className='dot'></span>
+                <span className='dot'></span>
+            </div>
+            <div className='column'>
+                <span className='dot'></span>
+            </div>
+            <div className='column'>
+                <span className='dot'></span>
+                <span className='dot'></span>
+            </div>
+        </button>
     )
 }
 
